25bb80e84c6fd3d8e2279147db6f69fbfe183a83
FelipeIgansi <lipeigansi@gmail.com>
2023-02-27 10:44:11 -0300

    Aula OOP - Overload e Overwrite

--- src/main/kotlin/Heranca/heranca.kt
+++ src/main/kotlin/Heranca/heranca.kt
@@ -3,6 +3,13 @@ package Heranca
 open class AnimalEstimacao(var tipo: String){
     protected lateinit var nome: String
     open fun fazSom(){}
+    open fun anda(qtnPatas: Int) {
+        if (qtnPatas == 2){
+            println("Anda em pé")
+        }else if (qtnPatas == 4){
+            println("Anda sob as 4 patas")
+        }
+    }
 }
 class caachorro(tipo: String):AnimalEstimacao(tipo){
     override fun fazSom(){
@@ -14,6 +21,16 @@ class caachorro(tipo: String):AnimalEstimacao(tipo){
     fun chamar(){
         println("Vem ${this.nome}")
     }
+    override fun anda(qtnPatas: Int){
+        correr()
+        super.anda(qtnPatas)// exemplo não é bem o ideal, mas foi o que pensei para exemplificar o super
+    }
+    // Overload é quando vc tem o mesmo nome, mas comportamentos diferentes
+    fun correr(){
+    }
+    private fun correr(patas:Int) {}
+    private fun correr(patas:Float) {}
+    private fun correr(patas:String) {}
 }
 
 
